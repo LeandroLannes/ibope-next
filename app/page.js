c3ee86985c047df42e23b1534da7e9d66c22ef61
LeandroLannes <leandro.nolasco@gmail.com>
2026-05-29 17:22:43 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -166,7 +166,7 @@ export default function App() {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "x-api-key": "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",
+        "x-api-key": "COLOQUE_SUA_CHAVE_AQUI",
         "anthropic-version": "2023-06-01",
         "anthropic-dangerous-direct-browser-access": "true"
       },
